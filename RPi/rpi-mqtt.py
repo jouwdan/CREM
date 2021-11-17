447c6106d24ba2968dc24795dc15341cab134a94
Jordan Harrison <contact@jordanharrison.ie>
2021-11-17 20:24:41 +0000

    Update rpi-mqtt.py

--- RPi/rpi-mqtt.py
+++ RPi/rpi-mqtt.py
@@ -4,14 +4,17 @@ import sys
 
 from datetime import datetime
 
+sensors = ['MDF1','MDF2']
+
 def on_connect(client, userdata, flags, rc):
-  print ('Connected with result code {0}'.format(rc))
-  client.subscribe('CREM_Sensor')
+  for sensor in sensors:
+    print ('Connected to ' + sensor + ' with result code {0}'.format(rc))
+    client.subscribe(sensor)
 
 def on_message(client, userdata, msg):
   temp, humidity = [float(x) for x in msg.payload.decode("utf-8").split(',')]
   timestamp = datetime.now()
-  print('Timestamp: ' + timestamp.strftime("%d/%m/%Y %H:%M:%S") + ', Temperature: ' + "{:.1f}".format(temp) + 'C, Humidity: ' + "{:.1f}".format(humidity) + '%')
+  print('Sensor: ' + msg.topic + ', Timestamp: ' + timestamp.strftime("%d/%m/%Y %H:%M:%S") + ', Temperature: ' + "{:.1f}".format(temp) + 'C, Humidity: ' + "{:.1f}".format(humidity) + '%')
   try:
     conn = mariadb.connect(
       database="cremdb",
@@ -25,16 +28,16 @@ def on_message(client, userdata, msg):
     sys.exit(1)
   cursor = conn.cursor()
   try:
-    cursor.execute("INSERT INTO readings (timestamp,reading,humidity) VALUES (?,?,?)", (timestamp,temp,humidity))
+    cursor.execute("INSERT INTO readings (sensor,timestamp,reading,humidity) VALUES (?,?,?,?)", (msg.topic,timestamp,temp,humidity))
   except mariadb.Error as e:
     print(f"Error: {e}")
   conn.commit()
   print("Successfully inserted to the database")
   conn.close()
   
+  
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect('localhost', 1883, 60)
-
 client.loop_forever()
