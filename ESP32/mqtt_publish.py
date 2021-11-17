6db9781e32a6d47eee5e00f3ca766533008a02f1
Jordan Harrison <contact@jordanharrison.ie>
2021-11-17 20:26:40 +0000

    Update mqtt_publish.py

--- ESP32/mqtt_publish.py
+++ ESP32/mqtt_publish.py
@@ -9,15 +9,15 @@ def do_connect():
     wlan.active(True)
     if not wlan.isconnected():
         print('connecting to network...')
-        wlan.connect('wifi-ssid', 'wifi-password')
+        wlan.connect('network-ssid', 'network-password')
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
 
 def get_readings():
-    CLIENT_ID = 'Sensor_Name'
+    CLIENT_ID = 'MDF1'
     SERVER = 'raspberrypi.local'
-    TOPIC = b'CREM_Sensor'
+    TOPIC = b'MDF1'
     client = MQTTClient(CLIENT_ID, SERVER)
     client.connect()
     sensor = DHT22(Pin(4, Pin.IN, Pin.PULL_UP))
@@ -27,8 +27,9 @@ def get_readings():
             temp = sensor.temperature()
             humidity = sensor.humidity()
             reading = (b'{0:3.1f},{1:3.1f}'.format(temp, humidity))
-            client.publish(TOPIC, reading)
             print(reading)
+            client.publish(TOPIC, reading)
+            print('Published Successfully')
         except OSError:
             print('Sensor reading failed')
         sleep(5)
