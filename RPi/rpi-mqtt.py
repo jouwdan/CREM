3d2b3e75cf4149d041eef23f15fc2bc0ffcf26f2
Jordan Harrison <76656637+jordharr@users.noreply.github.com>
2021-12-08 20:24:49 +0000

    smtp

--- RPi/rpi-mqtt.py
+++ RPi/rpi-mqtt.py
@@ -1,10 +1,28 @@
 import paho.mqtt.client as mqtt
 import mariadb
 import sys
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from datetime import datetime
 
 sensors = ['MDF1','MDF2']
+
+def send_mail(eFrom, to, subject, text):
+    smtpServer='your.smtp.server.org'
+    smtpUser='SMTP_USER'
+    smtpPassword='SMTP_PASSWORD'
+    port=587
+
+    msg = MIMEMultipart()
+    msg.attach(MIMEText(text))
+    msg['Subject'] = subject
+
+    s = smtplib.SMTP(smtpServer, port)
+    s.login(smtpUser, smtpPassword)
+    s.sendmail(eFrom, to, msg.as_string())
+    s.quit()
 
 def on_connect(client, userdata, flags, rc):
   for sensor in sensors:
@@ -15,6 +33,9 @@ def on_message(client, userdata, msg):
   temp, humidity = [float(x) for x in msg.payload.decode("utf-8").split(',')]
   timestamp = datetime.now()
   print('Sensor: ' + msg.topic + ', Timestamp: ' + timestamp.strftime("%d/%m/%Y %H:%M:%S") + ', Temperature: ' + "{:.1f}".format(temp) + 'C, Humidity: ' + "{:.1f}".format(humidity) + '%')
+  if temp >= 30 or temp <= 10:
+    text= f'Message from CREM,\n ' + msg.topic + ' has reached ' + "{:.1f}".format(temp) + ' degrees at ' + timestamp.strftime("%d/%m/%Y %H:%M:%S")
+    send_mail('crem@jordanharrison.ie', 'YOUR_EMAIL_ADDRESS', 'CREM heat alert',text)
   try:
     conn = mariadb.connect(
       database="cremdb",
